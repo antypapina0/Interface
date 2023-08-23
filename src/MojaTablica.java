3185d7cd6dcebdbed150f9442a7674cdf7a2456f
antypapina0 <sylwestrzak2000@gmail.com>
2023-08-23 16:05:51 +0200

    added coments

--- src/MojaTablica.java
+++ src/MojaTablica.java
@@ -8,7 +8,7 @@ public class MojaTablica implements Zbior {
     //to jest zmienna typu int o nazwie pozycja
     private int pozycja;
 
-    //konstruktor
+    //konstruktor do wywolania nowego obiektu tej klasu, argument rozmiar daje ile miejsc ma tablica, inicjuje tablica i zeruje wskaznic pozycja
     public MojaTablica(int rozmiar){
         this.mojeElementy = new int[rozmiar];
         this.pozycja = 0;
@@ -17,7 +17,7 @@ public class MojaTablica implements Zbior {
     //metoda do sprawdzenie czy item jest w tablicy
     @Override
     public boolean nalezy(int item) {
-        //
+        //przechodzi przez wszystkie pozycje jestli nalezy jest true, jezeli nie
         for (int i = 0; i < pozycja; i++) {
             if (mojeElementy[i] == item) {
                 return true;
@@ -27,6 +27,7 @@ public class MojaTablica implements Zbior {
     }
     //dodaniea elementu do tablicy
     @Override
+    //jezeli jest miejsce w tablicy i gdy itema nie ma w tablicy dodaje do siebie i wtedy miejsce pozycji jest zwiekszane aby wiedziec czy jest miejsce
     public void dodaj(int item) {
         if (pozycja < mojeElementy.length && !nalezy(item)) {
             mojeElementy[pozycja] = item;
@@ -35,12 +36,14 @@ public class MojaTablica implements Zbior {
     }
     //zwracanie ile jest elementow
     @Override
+    //zwraca ile jest elementow w tablicy
     public int ile() {
         return pozycja;
     }
     //wyswietlanie wszystkich elementow
     @Override
     public void wyswietl() {
+        //przechodzi przez wszystkie pozycje w tablicy i wyswietla wszystkie nasze mojeElementy
         for (int i = 0; i < pozycja; i++) {
             System.out.print(mojeElementy[i] + ", ");
         }
